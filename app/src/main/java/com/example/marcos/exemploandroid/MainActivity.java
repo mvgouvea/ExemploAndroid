714100eac70ecc3c1c45cf2583050ee0e7a55631
Marcos Vinícius Gouvêa <marcoskilf@gmail.com>
2017-05-16 00:47:14 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/marcos/exemploandroid/MainActivity.java
+++ app/src/main/java/com/example/marcos/exemploandroid/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//modificado pelo navegador
 public class MainActivity extends AppCompatActivity {
 
     @Override
